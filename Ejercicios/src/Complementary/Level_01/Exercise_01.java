a9c907d84fc7e92c62ec0425826130d28a2af79c
Dihue <dihue21@gmail.com>
2022-07-17 04:13:21 -0300

    Exercise correction

--- Ejercicios/src/Complementary/Level_01/Exercise_01.java
+++ Ejercicios/src/Complementary/Level_01/Exercise_01.java
@@ -1,6 +1,9 @@
 /*
 Solicitar por consola el nombre del usuario e imprimir por pantalla el siguiente
 mensaje "¡HOLA {USUARIO}!"
+
+Input: Obi Wan Kenobi
+Output: HOLA Obi Wan Kenobi!!!
 */
 package Complementary.Level_01;
 
@@ -8,20 +11,20 @@ import java.util.Scanner;
 
 public class Exercise_01 {
     public static void main(String[] args) {
-        // Declaracion de variables
+        // Declaración de variables
         String usuario;
 
-        // Creacion del objeto Scanner
+        // Creación del objeto Scanner
         Scanner scan = new Scanner(System.in);
 
         // Salida por pantalla con msj para el usuario
         System.out.println("Ingrese su nombre de usuario: ");
-        usuario = scan.next();
+        usuario = scan.nextLine();
 
-        // Salida por pantalla del msj con el resutlado del programa
-        System.out.println("¡HOLA " + usuario.toUpperCase() + " !");
+        // Salida por pantalla del msj con el resultado del programa
+        System.out.println("HOLA " + usuario + "!!!");
 
-        // Finalizacion Scanner
+        // Finalización Scanner
         scan.close();
     }
 }
